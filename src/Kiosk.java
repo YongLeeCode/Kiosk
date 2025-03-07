6a20b53b6c95fe00b6c8abbdb6ae73a1e632c228
YongLeeCode <yonghwan311@gmail.com>
2025-03-07 18:12:02 +0900

    MenuItem을 사용한 출력문 구현

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
     Scanner scanner = new Scanner(System.in);
 
     public void start() {
+        List<MenuItem> items = new ArrayList<>();
+        items.add(new MenuItem(1, "ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        items.add(new MenuItem(2, "SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        items.add(new MenuItem(3,"Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        items.add(new MenuItem(4, "Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         int choice = 1;
         while (choice != 0) {
-            displayMenu();
+            displayMenu(items);
             choice = scanner.nextInt();
             System.out.println(order(choice));
             System.out.println();
@@ -24,14 +32,11 @@ public class Kiosk {
         };
     }
 
-    private void displayMenu() {
-        System.out.println("""
-                [ SHAKESHACK MENU ]
-                1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
-                2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
-                3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
-                4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
-                0. 종료
-                """);
+    private void displayMenu(List<MenuItem> items) {
+        System.out.println("[ SHAKESHACK MENU ]");
+        items.stream().forEach(i ->
+                System.out.printf("%d. %s | W %f | %s \n", i.getId(), i.getName(), i.getPrice(), i.getDetail())
+        );
+        System.out.println("0. 종료 \n");
     }
 }
