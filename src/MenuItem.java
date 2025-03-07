66925bda64601e26e1a680835631c1fec272864b
YongLeeCode <yonghwan311@gmail.com>
2025-03-07 16:42:41 +0900

    feat: 음식 속성을 가진 클래스의 생성자 구현

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class MenuItem {
+    // ShackBurger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+    private String name;
+    private double price;
+    private List<String> explanation;
+
+    public MenuItem(String name, double price, List<String> explanation) {
+        this.name = name;
+        this.price = price;
+        this.explanation = explanation;
+    }
 }
