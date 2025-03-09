c3d18c2d629da4132c0fc8851a7e4287ec1df0a9
YongLeeCode <yonghwan311@gmail.com>
2025-03-09 22:06:29 +0900

    feat: 키오스크 시작 클래스 구현

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
@@ -5,33 +7,36 @@ public class Kiosk {
 
     public void start() {
         int choice = 1;
+        List<MenuItem> burgers = new ArrayList<>();
+
+        burgers.add(new MenuItem(1, "ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgers.add(new MenuItem(2, "SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgers.add(new MenuItem(3, "Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgers.add(new MenuItem(4, "Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         while (choice != 0) {
-            displayMenu();
+            displayMenu(burgers);
             choice = scanner.nextInt();
-            System.out.println(order(choice));
+            try {
+                order(burgers.get(choice - 1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("잘못된 입력입니다.");
+            }
+
             System.out.println();
         }
     }
 
-    private String order(int choice) {
-        return switch (choice) {
-            case 1 -> "선택된 메뉴 : ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거";
-            case 2 -> "선택된 메뉴 : SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거";
-            case 3 -> "선택된 메뉴 : Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거";
-            case 4 -> "선택된 메뉴 : Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거";
-            case 0 -> "프로그램을 종료합니다.";
-            default -> "잘못된 입력입니다.";
-        };
+    private void order(MenuItem burger) {
+        System.out.printf("선택된 메뉴 : %-15s  | W %f | %s \n", burger.getName(), burger.getPrice(), burger.getDetail());
     }
 
-    private void displayMenu() {
-        System.out.println("""
-                [ SHAKESHACK MENU ]
-                1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
-                2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
-                3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
-                4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
-                0. 종료
-                """);
+    private void displayMenu(List<MenuItem> burgers) {
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (int i = 0; i < burgers.size(); i++) {
+            MenuItem burger = burgers.get(i);
+            System.out.printf("%d. %-15s  | W %f | %s \n", burger.getId(), burger.getName(), burger.getPrice(), burger.getDetail());
+        }
+        System.out.println("0. 종료");
     }
 }
