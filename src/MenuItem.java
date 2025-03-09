464d17f9e0adc1cef0c54e08f3dda119f826a46d
YongLeeCode <yonghwan311@gmail.com>
2025-03-09 22:05:52 +0900

    feat: 메뉴아이템 클래스 게터 생성

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,14 +1,29 @@
-import java.util.List;
-
 public class MenuItem {
-    // ShackBurger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+    private int id;
     private String name;
     private double price;
-    private List<String> explanation;
+    private String detail;
 
-    public MenuItem(String name, double price, List<String> explanation) {
+    public MenuItem(int id, String name, double price, String detail) {
+        this.id = id;
         this.name = name;
         this.price = price;
-        this.explanation = explanation;
+        this.detail = detail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDetail() {
+        return detail;
     }
 }
