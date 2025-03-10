686a3de73ffffe465326a7930d4953690aa6bac1
YongLeeCode <yonghwan311@gmail.com>
2025-03-10 16:58:51 +0900

    feat: 메뉴 클래스 구현

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,23 @@
+import java.util.List;
+
 public class Menu {
+    private String category;
+    private List<MenuItem> items;
+
+    public Menu(String category, List<MenuItem> items) {
+        this.category = category;
+        this.items = items;
+    }
+
+    public MenuItem getItem(int itemNumber) {
+        return items.get(itemNumber - 1);
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
