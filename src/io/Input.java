36544ce27e2cbf71d4ed5ef2c09aa73ee9d29788
YongLeeCode <yonghwan311@gmail.com>
2025-03-11 16:22:20 +0900

    feat: 인풋 아웃풋 클래스 구현

--- src/io/Input.java
+++ src/io/Input.java
@@ -11,10 +11,30 @@ public class Input {
         try {
             return scanner.nextInt();
         } catch (InputMismatchException e) {
-            System.out.println("메뉴: 번호만 입력 가능합니다.");
+            System.out.println("번호만 입력 가능합니다.");
             scanner.nextLine();
             return getNumber();
         }
     }
 
+    public int getItemNumber(int size) {
+        try {
+            int itemNumber = scanner.nextInt();
+            if((itemNumber >= 1 && itemNumber <= size) || itemNumber == 0) {
+                return itemNumber;
+            }
+            throw new IndexOutOfBoundsException("선택사항에 없는 번호입니다.");
+
+        } catch (InputMismatchException e) {
+            System.out.println("번호만 입력 가능합니다.");
+            System.out.println("메뉴판에 있는 번호를 다시 선택해주세요.");
+            scanner.nextLine();
+            return getItemNumber(size);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+            System.out.println("메뉴판에 있는 번호를 다시 선택해주세요.");
+            return getItemNumber(size);
+        }
+    }
 }
+
