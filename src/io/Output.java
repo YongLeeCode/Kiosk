d14f766cfbb383fa88345efe007f38aee27c62c3
YongLeeCode <yonghwan311@gmail.com>
2025-03-12 16:04:40 +0900

    feat,refactor: 가독성을 위한 리팩토링 및 카트 클래스를 구현하며 필요한 출력 구현

--- src/io/Output.java
+++ src/io/Output.java
@@ -1,30 +1,78 @@
 package io;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import menu.MenuItem;
 
 public class Output {
-    public void displayMenus() {
-        System.out.println("[ MENU ]");
-        System.out.println("1. Burgers");
-        System.out.println("2. Drinks");
-        System.out.println("3. Desserts");
-        System.out.println("0. 종료");
+    public void displayMainMenu() {
+        System.out.println("""
+                \n-------------
+                [ MENU ]
+                1. Burgers
+                2. Drinks
+                3. Desserts
+                0. 종료
+                -------------""");
+    }
+
+    public void displayOrderMenu(Integer orderQuantity) {
+        if (!orderQuantity.equals(0)) {
+            System.out.println("""
+            -------------
+            [ Order Menu ]
+            4. Order
+            5. Cancel
+            -------------
+            """);
+        }
+    }
+
+    public void displayTotalPrice(double total) {
+        System.out.println("\n-------------");
+        System.out.println("[ Total ]");
+        System.out.println("W " + total);
+        System.out.println("-------------");
+    }
+
+    public void displayCartItems(HashMap<MenuItem, Integer> items) {
+        System.out.println("\n-------------");
+        System.out.println("[ Orders ]");
+        for (MenuItem key : items.keySet()) {
+            System.out.printf("%s %f 수량: %d\n", key.getName(), key.getPrice(), items.get(key));
+        }
+        System.out.println("-------------");
     }
 
     public void displayMenuItems(String category, List<MenuItem> items) {
-        System.out.printf("\n[ %s MENU ] \n", category);
+        System.out.println("\n-------------");
+        System.out.printf("[ %s MENU ] \n", category);
         IntStream.range(0, items.size())
-                        .forEach(i -> {
-                            MenuItem item = items.get(i);
-                            System.out.printf("%d %s : %f | %s | \n", i + 1, item.getName(), item.getPrice(), item.getDetail());
-                        });
+                .forEach(i -> {
+                    MenuItem item = items.get(i);
+                    System.out.printf("%d %s : %f | %s | \n", i + 1, item.getName(), item.getPrice(), item.getDetail());
+                });
         System.out.println("0. 메인 메뉴로 돌아가기");
+        System.out.println("-------------");
     }
 
-     public void displayOrder(MenuItem burger) {
+    public void displayClearCart() {
+        System.out.println("\n-------------");
+        System.out.println("장바구니를 모두 비웠습니다.");
+        System.out.println("-------------");
+    }
+
+    public void displayOrder(MenuItem burger) {
+        System.out.println("\n-------------");
         System.out.printf("선택된 메뉴 : %-15s  | W %f | %s \n", burger.getName(), burger.getPrice(), burger.getDetail());
+        System.out.println("-------------");
+    }
+
+    public void displayResult(double total) {
+        System.out.println("\n-------------");
+        System.out.printf("주문이 완료되었습니다. 금액은 W %f 입니다.\n", total);
+        System.out.println("-------------");
     }
 }
