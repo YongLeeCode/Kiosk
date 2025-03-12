18d67c0200ccbb0cf12290c2766e3d8edd7db6bd
YongLeeCode <yonghwan311@gmail.com>
2025-03-12 20:24:26 +0900

    feat: Discount enum클래스 생성

---
+++ src/enums/Discount.java
@@ -0,0 +1,14 @@
+package enums;
+
+/**
+ *packageName    : enums
+ * fileName       : Discount
+ * author         : yong
+ * date           : 3/12/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/12/25        yong       최초 생성
+ */public enum Discount {
+}
