a63b46ba171513b4b802d66672e0fecbb2cc34f1
YongLeeCode <yonghwan311@gmail.com>
2025-03-14 13:44:23 +0900

    feat: 카테고리 enum 클래스 제거 및 액션 enum 클래스 생성

---
+++ src/kiosk/enums/Action.java
@@ -0,0 +1,14 @@
+package kiosk.enums;
+
+/**
+ *packageName    : kiosk.enums
+ * fileName       : Action
+ * author         : yong
+ * date           : 3/14/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/14/25        yong       최초 생성
+ */public class Action {
+}
